ac389ef0e1ef738facd2cdb22f9ec41020ea6725
Nicolas Savoire <nicolas.savoire@datadoghq.com>
2026-03-23 11:05:50 +0000

    Test codeql warning fix

--- processmanager/processinfo.go
+++ processmanager/processinfo.go
@@ -788,7 +788,13 @@ func (pm *ProcessManager) ProcessedUntil(traceCaptureKTime times.KTime) {
 }
 
 func readProcessContext(mapping *process.Mapping, pr process.Process, oldProcessContextInfo processcontext.Info) processcontext.Info {
-	ctxInfo, err := processcontext.Read(libpf.Address(mapping.Vaddr), pr.GetRemoteMemory(), oldProcessContextInfo.PublishedAtNs)
+	//
+	// addr := libpf.Address(mapping.Vaddr & uint64(^libpf.Address(0)))
+	var addr libpf.Address
+	if mapping.Vaddr < uint64(^libpf.Address(0)) {
+		addr = libpf.Address(mapping.Vaddr)
+	}
+	ctxInfo, err := processcontext.Read(addr, pr.GetRemoteMemory(), oldProcessContextInfo.PublishedAtNs)
 	if err == nil {
 		return ctxInfo
 	}
