e5afd75c3b5ab27d8feb61cdc214e0f644e7c519
Martin Levesque <martin.levesque@datadoghq.com>
2026-06-24 09:13:07 +0000

    fix: replace CGO_ENABLED check with runtime.iscgo

--- interpreter/golabels/golabels.go
+++ interpreter/golabels/golabels.go
@@ -25,6 +25,7 @@ type data struct {
 }
 
 var errDecodeSymbol = errors.New("failed to decode symbol")
+var errRuntimeIsCgoUnavailable = errors.New("runtime.iscgo value unavailable")
 
 func (d *data) String() string {
 	return "Golang labels " + d.goVersion
@@ -90,7 +91,7 @@ func Loader(_ interpreter.EbpfHandler, info *interpreter.LoaderInfo) (interprete
 	switch {
 	case errors.Is(err, libpf.ErrSymbolNotFound):
 		return nil, fmt.Errorf("failed to lookup symbol in %s: %v", info.FileName(), err)
-	case errors.Is(err, errDecodeSymbol):
+	case errors.Is(err, errDecodeSymbol), errors.Is(err, errRuntimeIsCgoUnavailable):
 		log.Warnf("In %s: %v", info.FileName(), err)
 	case errors.Is(err, nil):
 		// Nothing to do - just continue
